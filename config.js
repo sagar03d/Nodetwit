875eb79d7752e7252fbb5b253eb9023e5650db13
Sagar Dolui <sagar03d@gmail.com>
2018-12-04 17:55:31 +0530

    First Commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'i6NPrvsnwq5Dex39lgMzAGTp1',
-  consumer_secret:      'mQvRzJk5sVjo5Ucemo68WwrQqGASpUvEcyHoiJf4amm2gwSI8W',
-  access_token:         '940567004527783936-Gk0L8hZC8WDD1GMKn4sCWfVhAwHMXKD',
-  access_token_secret:  'tAE0Hm0yZHZAeANw2VCL5Xh7YyT8H1IoHzSH3nt7O6VNM'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
